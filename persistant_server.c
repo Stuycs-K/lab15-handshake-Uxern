71673235a5ed73f58f4e58f6dfb7a5f83df1f7d9
Chris Lam <clam50@stuy.edu>
2025-01-06 23:47:34 -0500

    finished persistant_server kinda

--- persistant_server.c
+++ persistant_server.c
@@ -1,7 +1,47 @@
 #include "pipe_networking.h"
+#define WKP "mario"
 
+int server_running = 1;
+
+static void sighandler(int signo) {
+    if ( signo == SIGINT ){
+		remove(WKP);
+		server_running = 0;	
+    }
+    else if ( signo == SIGPIPE ){
+    
+	}
+}
+    
 int main() {
-  int to_client;
-  int from_client;
-  from_client = server_handshake( &to_client );
+  srand(time(NULL));
+  signal(SIGINT, sighandler);
+  signal(SIGPIPE, SIG_IGN);
+
+  while (server_running) {
+	  
+	int to_client;
+	int from_client;
+	printf("Created mario pipe, waiting for client...\n");
+        from_client = server_setup();
+        if (from_client == -1) {
+            perror("server setup");
+            continue;
+        }	
+        if (server_handshake(&to_client) == -1) {
+            perror("handshake");
+            close(from_client);
+            continue;
+        }  
+		while (server_running){
+	int random_number = rand() % 101;
+	if(write(to_client, &random_number, sizeof(random_number)) < 0){
+		break;//exit(0);
+	}
+	sleep(1);
+  }
+	close(to_client);
+	close(from_client);
+  }
+
 }
