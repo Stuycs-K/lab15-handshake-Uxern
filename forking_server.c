28ad2f7b07654d26344ef34d047804d21a7360d6
Chris Lam <clam50@stuy.edu>
2025-01-07 00:28:29 -0500

    promising

--- forking_server.c
+++ forking_server.c
@@ -19,27 +19,28 @@ int main() {
     signal(SIGPIPE, SIG_IGN);
     while (server_running) {
         int to_client, from_client;
-        
-        printf("Created mario pipe and waiting for a client...\n");
+        //	printf("Created mario pipe and waiting for a client...\n");
         from_client = server_setup();
         if (from_client == -1) {
             perror("server setup");
             continue;
         }
-		pid_t p = fork();
-			if(p < 0){
-			printf("%s\n",strerror(errno));
-			}
-		else if(p == 0){
-	remove(WKP);
-   if (server_handshake(&to_client) == -1) {
-        perror("handshake failed");
-        close(from_client);
-        exit(1);
-    }
+	pid_t p = fork();
+	if(p < 0){
+		printf("%s\n",strerror(errno));
+	}
+	else if(p == 0){
+		remove(WKP);
+		if (server_handshake(&to_client) == -1) {
+			perror("handshake failed");
+			close(from_client);
+			exit(1);
+		}
     printf("Sub - Handshake successful. \n");
     while (1) {
         int random_number = rand() % 101;  
+        printf("Sending: %d\n", random_number);
+
         if (write(to_client, &random_number, sizeof(random_number)) == -1) {
             perror("Client disconnected?");
             break;
@@ -48,7 +49,6 @@ int main() {
     }
 	close(to_client);
     close(from_client);
-	exit(0);
+		}
 	}
-}
 }
